020b3cdd77f0f2e5c4640877c5ca4a55b43368e8
John Kusching <kimshrum@Kims-Air.home>
2018-07-18 18:18:32 -0500

    Created sessions in the server.js file

--- server.js
+++ server.js
@@ -3,13 +3,33 @@ const express = require("express");
 const app = express();
 const port = 3000;
 const mongoose = require("mongoose");
-// const session = require("express-session");
+const session = require("express-session");
 
 //Sessions
+app.use(session({
+  secret: "feedmeseymour",
+  resave: false,
+  saveUninitialized: false
+}));
 
 //Middleware
 app.use(express.json());
 app.use(express.static("public"));
+
+//Get Routes
+//Route checks to see if the user is logged in
+//If the user is logged in, send the user from the current session as a response
+//If the user is NOT logged in, send an error message
+app.get("/log", (req, res) => {
+  if(req.session.currentUser){
+    res.json(req.session.currentUser);
+  } else {
+    res.status(401).json({
+      status: 401,
+      message: "Not logged in"
+    });
+  }
+});
 
 //Controllers
 const bookController = require("./controllers/books.js");
@@ -17,6 +37,9 @@ app.use("/books", bookController);
 
 const userController = require("./controllers/users.js");
 app.use("/users", userController);
+
+const sessionsController = require("./controllers/sessions.js");
+app.use("/sessions", sessionsController);
 
 //Listening Routes
 app.listen(port, () => {
