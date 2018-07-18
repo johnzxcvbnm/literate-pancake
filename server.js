8ba9199bb23341bb7191a6937712f251e4a45c06
John Kusching <kimshrum@Kims-Air.home>
2018-07-18 16:23:17 -0500

    Created GET and POST routes

--- server.js
+++ server.js
@@ -1,11 +1,32 @@
+//Dependancies
 const express = require("express");
 const app = express();
 const port = 3000;
+const mongoose = require("mongoose");
+// const session = require("express-session");
 
-app.get("/", (req, res) => {
-  res.send("HELLO WORLD");
-});
+//Sessions
 
+//Middleware
+app.use(express.json());
+app.use(express.static("public"));
+
+//Controllers
+const bookController = require("./controllers/books.js");
+app.use("/books", bookController);
+
+//Get Routes
+// app.get("/", (req, res) => {
+//   res.send("HELLO WORLD");
+// });
+
+//Listening Routes
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
+});
+
+//Mongoose Connection
+mongoose.connect("mongodb://localhost:27017/BookCat", { useNewUrlParser: true } );
+mongoose.connection.once("open", () => {
+  console.log("Connected to mongoose!");
 });
